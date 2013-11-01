9e1cf9c9359bd219ffbefcce3c7fe12842816711
MrXiao <amoymythbuster@gmail.com>
2013-11-01 10:26:09 +0800

    second commit_add_commit

---
+++ js/subtitle_player_js.js
@@ -0,0 +1,12 @@
+function pauseVideo() {
+    player.pauseVideo();
+}
+function seekTo(s) {
+    player.seekTo(s);
+}
+function playVideo() {
+    player.playVideo();
+}
+function currentTime() {
+    return player.currentTime();
+}
